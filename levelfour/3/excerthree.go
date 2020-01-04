81b9f01ce4320995bdceb7ac3035e559f80e6135
filodoks <filodoks@gmail.com>
2020-01-04 13:40:11 +0100

    6-10

--- levelfour/3/excerthree.go
+++ levelfour/3/excerthree.go
@@ -11,5 +11,18 @@ func main() {
 	fmt.Println(x)
 
 	y := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	y = append(y, 52)
 	fmt.Println(y)
+	y = append(y, 53, 54, 55)
+	fmt.Println(y)
+
+	z := []int{56, 57, 58, 59, 60}
+	y = append(y, z...)
+	fmt.Println(y)
+
+	// excercize 5
+	a := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	fmt.Println(a)
+	a = append(a[:3], a[6:]...)
+	fmt.Println(a)
 }
